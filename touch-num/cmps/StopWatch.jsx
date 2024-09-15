73758f52805438d4df156518cef5d1bf646010c1
aviavi16 <eranis54321@gmail.com>
2024-09-15 22:48:08 +0300

    added stopwatch component

--- touch-num/cmps/StopWatch.jsx
+++ touch-num/cmps/StopWatch.jsx
@@ -1,7 +1,36 @@
-export function StopWatch(){
+import { useEffect, useRef, useState } from "react";
+import "../css/stop-watch.css";
+
+export function StopWatch( { time } ){
+    const [timer, setTimer] = useState(time);
+    const [isRunning, setIsRunning] = useState(false);
+    let  timeInterval = useRef(null)
+
+    useEffect(() => {
+        setTimer(time)
+    }, [ time ])
+        
+    const formatTime = (timer) => {
+        const minutes = Math.floor(timer / 6000).toString().padStart(2, "0");
+        const seconds = Math.floor((timer / 100) % 60).toString().padStart(2, "0");
+      
+        return { minutes, seconds };
+    };
+      
+    const { minutes, seconds } = formatTime(timer);
+
     return (
         <section className="stopWatch-container">
-            <span> stopWatch </span>
+            <h2 > Time: </h2>
+            <div className="timer-container">
+                <div className="timer-box">
+                    <h1>{minutes}</h1>
+                </div>
+                <span className="colon">:</span>
+                <div className="timer-box">
+                    <h1>{seconds}</h1>
+                </div>
+            </div>
         </section>
     )
 }
--- touch-num/src/App.jsx
+++ touch-num/src/App.jsx
@@ -1,72 +1,81 @@
+import { useRef, useState } from "react";
 import { StopWatch } from "../cmps/StopWatch";
-var gDifficulty = 'Easy'
 
-function startGame(){
-    if (document.querySelector('.difficulty-container'))
-        gDifficulty = document.querySelector('.difficulty-container').value;
-    deleteOlderGame();
-
-    var size = 0;
-    console.log('gDifficulty:', gDifficulty)
-    switch (gDifficulty){
-        case 'Medium':
-            size = 25;
-            break;
-        case 'Hard':
-            size = 36;
-            break;
-        case 'Easy': 
-        default:
-            size = 16;
-            break;
-    }
-    console.log('gDifficulty:', size)
-
-    var div = document.querySelector('.myDynamicTable');
-    var table = document.createElement('table');
-    table.className= 'table';
-    var tableBody = document.createElement('tbody');
-    var numberOfRowCols = Math.sqrt(size);
-
-    var allNums = Array(size).fill().map((_, i) => i * 1);
-    shuffleArray(allNums);
-
-    for (var i = 0; i < numberOfRowCols; i++) {
-        var tr = document.createElement('tr');
-        tableBody.appendChild(  tr   );
-        for (var j = 0; j < numberOfRowCols; j++) {
-            var td =  document.createElement('td');
-            td.className = 'td';
-            td.onclick =  function (evt) { choose(this); }
-            var span = document.createElement('span');
-
-            span.innerText =  allNums.pop();
-            td.appendChild(span) 
-                tr.appendChild(  td     ) ;
-        }
-    }
-    table.appendChild (tableBody);
-    div.appendChild (table);
-
-    onCloseModal();
-
-}
-
-function shuffleArray(array) {
-    for (let i = array.length - 1; i >= 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-}
-
-function deleteOlderGame(){
-    var div = document.querySelector('.myDynamicTable');
-    if (div)
-        div.innerHTML = ''
-
-}
 
 export function App() {
+    const [ gameStarted, setGameStarted ] = useState( false )
+    const [ timer, setTimer ] = useState( 0 )
+    const [ nextNum , setNextNum ] = useState( 0 )
+
+    let  timeInterval = useRef(null)
+
+    var gDifficulty = 'Easy'
+
+    function startGame(){
+        if (document.querySelector('.difficulty-container'))
+            gDifficulty = document.querySelector('.difficulty-container').value;
+        deleteOlderGame();
+        handleReset();
+
+        var size = 0;
+        console.log('gDifficulty:', gDifficulty)
+        switch (gDifficulty){
+            case 'Medium':
+                size = 25;
+                break;
+            case 'Hard':
+                size = 36;
+                break;
+            case 'Easy': 
+            default:
+                size = 16;
+                break;
+        }
+        console.log('gDifficulty:', size)
+    
+        var div = document.querySelector('.myDynamicTable');
+        var table = document.createElement('table');
+        table.className= 'table';
+        var tableBody = document.createElement('tbody');
+        var numberOfRowCols = Math.sqrt(size);
+    
+        var allNums = Array(size).fill().map((_, i) => i * 1);
+        shuffleArray(allNums);
+    
+        for (var i = 0; i < numberOfRowCols; i++) {
+            var tr = document.createElement('tr');
+            tableBody.appendChild(  tr   );
+            for (var j = 0; j < numberOfRowCols; j++) {
+                var td =  document.createElement('td');
+                td.className = 'td';
+                td.onclick =  function (evt) { choose(this); }
+                var span = document.createElement('span');
+    
+                span.innerText =  allNums.pop();
+                td.appendChild(span) 
+                    tr.appendChild(  td     ) ;
+            }
+        }
+        table.appendChild (tableBody);
+        div.appendChild (table);
+        handleStart();
+        onCloseModal();
+    
+    }
+    
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i >= 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+    
+    function deleteOlderGame(){
+        var div = document.querySelector('.myDynamicTable');
+        if (div)
+            div.innerHTML = ''
+    
+    }
 
     function choose(cell){
         console.log('cell:', cell)
@@ -95,6 +104,25 @@ export function App() {
     }
 
 
+    const handleStart = () => {
+        setGameStarted(true);
+        setTimer(0);
+        timeInterval.current = setInterval(() => {
+            setTimer((prev) => prev + 1);
+        }, 10);
+    };
+
+    const handlePause = () => {
+        if (!gameStarted) return;
+        setGameStarted(false);
+        clearInterval(timeInterval.current);
+    };
+        
+    const handleReset = () => {
+        setGameStarted(false);
+        clearInterval(timeInterval.current);
+        setTimer(0);
+    };
 
 
     return (
@@ -131,7 +159,8 @@ export function App() {
                 
             </dialog>
 
-            <StopWatch />
+            <StopWatch time ={ timer }/>
+            <span >     the next number is : {nextNum} </span>
 
             <div className="myDynamicTable"></div>
         </section>
