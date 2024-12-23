42606f6a614e29d4ac3d2285bdc4e371059e2288
Avinoam <eranis54321@gmail.com>
2024-12-23 16:49:52 +0200

    Hint: added picture

--- touch-num/src/cmps/Hint.jsx
+++ touch-num/src/cmps/Hint.jsx
@@ -1,5 +1,8 @@
 export function Hint( { nextNum } ){
     return (
-        <div className="hint-container">     the next number is : {nextNum} </div>
+        <div className="hint-container">     
+            <img src="/touch-nums/src/imgs/hint.png" className="image-hint"/>
+            the next number is : {nextNum} 
+        </div>
     )
 }
