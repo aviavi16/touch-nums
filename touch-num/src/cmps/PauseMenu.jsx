03b78f3e95563a908ac40f275814bce9d0fe6459
Avinoam <eranis54321@gmail.com>
2025-01-26 10:48:14 +0200

    options menu display

--- touch-num/src/cmps/PauseMenu.jsx
+++ touch-num/src/cmps/PauseMenu.jsx
@@ -10,18 +10,26 @@ export function PauseMenu({onClosePause, restart } ){
                             OPTIONS
                         </button>
                     </div>
-                    <div className="pause-menu-music">
-                        <span> Music </span>
-                        <CustomCheckbox />
+                    <div className="options-display-container">
+                        <div className="pause-menu-music">
+                            <span> Music </span>
+                            <CustomCheckbox />
+                        </div>
+                        <div className="pause-menu-effects">
+                            <span> Effects </span>
+                            <CustomCheckbox />
+                        </div>
+                        <br/>
+                        <div className="pause-menu-restart">
+                            <button name="start" type="button" onClick={restart} className="restart-btn" >  Restart </button>
+                        </div>
                     </div>
-                    <div className="pause-menu-effects">
-                        <span> Effects </span>
-                        <CustomCheckbox />
+                    <div className="options-resume-container">
+                        <div className="options-resume-sub-container">
+                            <img  onClick={onClosePause} src={resumeBtn} height="2848" width="4288" className="resume-button" />
+                        </div>
                     </div>
-                    <div className="pause-menu-restart">
-                        <button name="start" type="button" onClick={restart} className="new-game-btn" >  Restart </button>
-                    </div>
-                    <img  onClick={onClosePause} src={resumeBtn} height="2848" width="4288" className="resume-button" />
+                    
                     {/* <button name="continue" onClick={onClosePause} className="resume-button"> Continue </button>  */}
                 </div>
                 {/* <img src={Bg} height="2848" width="4288" className="pause-menu-container-img" /> */}
